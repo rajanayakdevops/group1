053235f741227d056831e832c6f91623d059463d
rajanayakdevops <rajanayak.devops@gmail.com>
2025-11-23 14:54:44 +0530

    updated the side bar into components

---
+++ src/components/java-components/sidebarData.ts
@@ -0,0 +1,59 @@
+export const sidebarData = [
+  {
+    parent: 'introduction',
+    label: 'Java Introduction',
+    children: [
+      { label: 'Get Started With Java', link: '/docs/java/introduction/get-started' },
+      { label: 'Your First Java Program', link: '/docs/java/introduction/first-program' },
+      { label: 'Java Comments', link: '/docs/java/introduction/comments' },
+    ],
+  },
+  {
+    parent: 'fundamentals',
+    label: 'Java Fundamentals',
+    children: [
+      { label: 'Variables', link: '/docs/java/topics/variables' },
+      { label: 'Loops', link: '/docs/java/topics/loops' },
+    ],
+  },
+  {
+    parent: 'flow',
+    label: 'Java Flow Control',
+    children: [
+      { label: 'If-Else', link: '/docs/java/flow/if-else' },
+      { label: 'Switch', link: '/docs/java/flow/switch' },
+      { label: 'Loops', link: '/docs/java/flow/loops' },
+    ],
+  },
+  {
+    parent: 'arrays',
+    label: 'Java Arrays',
+    children: [
+      { label: 'What are Arrays?', link: '/docs/java/arrays/intro' },
+      { label: 'Types of Arrays', link: '/docs/java/arrays/types' },
+      { label: 'Multidimensional Arrays', link: '/docs/java/arrays/multidimensional' },
+    ],
+  },
+  {
+    parent: 'oop',
+    label: 'Java OOP (I)',
+    children: [
+      { label: 'Classes & Objects', link: '/docs/java/oop/classes' },
+      { label: 'Inheritance', link: '/docs/java/oop/inheritance' },
+      { label: 'Polymorphism', link: '/docs/java/oop/polymorphism' },
+      { label: 'Abstraction', link: '/docs/java/oop/abstraction' },
+      { label: 'Encapsulation', link: '/docs/java/oop/encapsulation' },
+    ],
+  },
+  {
+    parent: 'oop2',
+    label: 'Java OOP (I)',
+    children: [
+      { label: 'Classes & Objects', link: '/docs/java/oop/classes' },
+      { label: 'Inheritance', link: '/docs/java/oop/inheritance' },
+      { label: 'Polymorphism', link: '/docs/java/oop/polymorphism' },
+      { label: 'Abstraction', link: '/docs/java/oop/abstraction' },
+      { label: 'Encapsulation', link: '/docs/java/oop/encapsulation' },
+    ],
+  },
+];
