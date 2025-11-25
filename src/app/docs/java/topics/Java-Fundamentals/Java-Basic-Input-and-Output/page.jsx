f9d39b3e1e18bb4279592ff31aaa889139ca6b05
rajanayakdevops <rajanayak.devops@gmail.com>
2025-11-25 10:18:45 +0530

    added all the content inside java fundamental

--- src/app/docs/java/topics/Java-Fundamentals/Java-Basic-Input-and-Output/page.jsx
+++ src/app/docs/java/topics/Java-Fundamentals/Java-Basic-Input-and-Output/page.jsx
@@ -1,11 +1,217 @@
-export default function VariablePage() {
+export default function JavaBasicInputOutputContent() {
   return (
-    <div className="prose prose-neutral max-w-none">
-      <h1 className="text-3xl font-bold mb-4">Java Variables</h1>
+    <div className="editor-contents text-black">
+      <h1 className="text-3xl font-bold mb-6">Java Basic Input and Output</h1>
+      <div className="content space-y-4">
 
-      <p>
-       Welcome to java basic input and output
-      </p>
+        <h2 id="output" className="text-2xl font-semibold mt-6 mb-2">Java Output</h2>
+
+        <p>In Java, you can simply use</p>
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto"><code>System.out.println(); or
+
+System.out.print(); or
+
+System.out.printf();</code></pre>
+        <p>to send output to standard output (screen).</p>
+
+        <p>Here,</p>
+        <ul className="list-disc list-inside">
+          <li><code>System</code> is a class</li>
+          <li><code>out</code> is a <code>public</code> <code>static</code> field: it accepts output data.</li>
+        </ul>
+
+        <p>Don't worry if you don't understand it. We will discuss <code>class</code>, <code>public</code>, and <code>static</code> in later chapters.</p>
+
+        <p>Let's take an example to output a line.</p>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class AssignmentOperator {
+  public static void main(String[] args) {
+      System.out.println("Java programming is interesting.");   
+  }
+}`}</code>
+        </pre>
+
+        <p><strong>Output</strong>:</p>
+        <pre className="bg-gray-200 p-3 rounded overflow-auto"><code>Java programming is interesting.</code></pre>
+
+        <p>Here, we have used the <code>println()</code> method to display the string.</p>
+
+        <hr className="my-6 border-gray-300" />
+        <h3 id="difference-print-println-printf" className="text-xl font-semibold mt-6 mb-2">Difference between println(), print() and printf()</h3>
+
+        <ul className="list-disc list-inside">
+          <li><code>print()</code> - It prints string inside the quotes.</li>
+          <li><code>println()</code> - It prints string inside the quotes similar like <code>print()</code> method. Then the cursor moves to the beginning of the next line.</li>
+          <li><code>printf()</code> - It provides string formatting (similar to <a href="/cpp-programming/library-function/cstdio/printf">printf in C/C++ programming</a>).</li>
+        </ul>
+
+        <hr className="my-6 border-gray-300" />
+        <h3 className="text-xl font-semibold mt-6 mb-2">Example: print() and println()</h3>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class Output {
+  public static void main(String[] args) {
+      System.out.println("1. println ");
+      System.out.println("2. println ");
+      
+      System.out.print("1. print ");
+      System.out.print("2. print");
+  }
+}`}</code>
+        </pre>
+
+        <p><strong>Output</strong>:</p>
+        <pre className="bg-gray-200 p-3 rounded overflow-auto"><code>{`1. println 
+2. println 
+1. print 2. print`}</code></pre>
+
+        <p>In the above example, we have shown the working of the <code>print()</code> and <code>println()</code> methods. To learn about the <code>printf()</code> method, visit <a href="https://www.cs.colostate.edu/~cs160/.Summer16/resources/Java_printf_method_quick_reference.pdf">Java printf()</a>.</p>
+
+        <hr className="my-6 border-gray-300" />
+        <h3 id="print-variables" className="text-xl font-semibold mt-6 mb-2">Example: Printing Variables and Literals</h3>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class Variables {
+  public static void main(String[] args) {
+      Double number = -10.6;
+      
+      System.out.println(5);
+      System.out.println(number);
+  }
+}`}</code>
+        </pre>
+
+        <p>When you run the program, the output will be:</p>
+
+        <pre className="bg-gray-200 p-3 rounded overflow-auto"><code>{`5
+-10.6`}</code></pre>
+
+        <p>Here, you can see that we have not used the quotation marks. It is because to display integers, <a href="/java-programming/variables-literals">variables</a> and so on, we don't use quotation marks.</p>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h3 id="print-strings" className="text-xl font-semibold mt-6 mb-2">Example: Print Concatenated Strings</h3>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class PrintVariables {
+  public static void main(String[] args) {
+      Double number = -10.6;
+      
+      System.out.println("I am " + "awesome.");
+      System.out.println("Number = " + number);
+  }
+}`}</code>
+        </pre>
+
+        <p><strong>Output</strong>:</p>
+        <pre className="bg-gray-200 p-3 rounded overflow-auto"><code>{`I am awesome.
+Number = -10.6`}</code></pre>
+
+        <p>In the above example, notice the line,</p>
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto"><code>{`System.out.println("I am " + "awesome.");`}</code></pre>
+
+        <p>Here, we have used the <code>+</code> operator to concatenate (join) the two strings: <var>"I am "</var> and <var>"awesome."</var>.</p>
+
+        <p>And also, the line,</p>
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto"><code>{`System.out.println("Number = " + number);`}</code></pre>
+
+        <p>Here, first the value of variable <var>number</var> is evaluated. Then, the value is concatenated to the string: <var>"Number = "</var>.</p>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h2 id="input" className="text-2xl font-semibold mt-6 mb-2">Java Input</h2>
+
+        <p>Java provides different ways to get input from the user. However, in this tutorial, you will learn to get input from user using the object of <code>Scanner</code> class.</p>
+
+        <p>In order to use the object of <code>Scanner</code>, we need to import <code>java.util.Scanner</code> package.</p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto"><code>{`import java.util.Scanner;`}</code></pre>
+
+        <p>To learn more about importing packages in Java, visit <a href="/java-programming/packages-import">Java Import Packages</a>.</p>
+
+        <p>Then, we need to create an object of the <code>Scanner</code> class. We can use the object to take input from the user.</p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto"><code>{`// create an object of Scanner
+Scanner input = new Scanner(System.in);
+
+// take input from the user
+int number = input.nextInt();`}</code></pre>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h3 id="integer-input" className="text-xl font-semibold mt-6 mb-2">Example: Get Integer Input From the User</h3>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`import java.util.Scanner;
+
+class Input {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("You entered " + number);
+
+        // closing the scanner object
+        input.close();
+    }
+}`}</code>
+        </pre>
+
+        <p><strong>Output</strong>:</p>
+        <pre className="bg-gray-200 p-3 rounded overflow-auto"><code>Enter an integer: 23
+You entered 23</code></pre>
+
+        <p>In the above example, we have created an object named <var>input</var> of the <code>Scanner</code> class. We then call the <code>nextInt()</code> method of the <code>Scanner</code> class to get an integer input from the user.</p>
+
+        <p>Similarly, we can use <code>nextLong()</code>, <code>nextFloat()</code>, <code>nextDouble()</code>, and <code>next()</code> methods to get <code>long</code>, <code>float</code>, <code>double</code>, and <code>string</code> input respectively from the user.</p>
+
+        <div className="bg-yellow-100 p-4 rounded-lg border border-yellow-300">
+          <strong>Note</strong>: We have used the <code>close()</code> method to close the object. It is recommended to close the scanner object once the input is taken.
+        </div>
+
+        <hr className="my-6 border-gray-300" />
+        <h3 id="double-string-input" className="text-xl font-semibold mt-6 mb-2">Example: Get float, double and String Input</h3>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`import java.util.Scanner;
+
+class Input {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        // Getting float input
+        System.out.print("Enter float: ");
+        float myFloat = input.nextFloat();
+        System.out.println("Float entered = " + myFloat);
+        
+        // Getting double input
+        System.out.print("Enter double: ");
+        double myDouble = input.nextDouble();
+        System.out.println("Double entered = " + myDouble);
+        
+        // Getting String input
+        System.out.print("Enter text: ");
+        String myString = input.next();
+        System.out.println("Text entered = " + myString);
+    }
+}`}</code>
+        </pre>
+
+        <p><strong>Output</strong>:</p>
+        <pre className="bg-gray-200 p-3 rounded overflow-auto"><code>{`Enter float: 2.343
+Float entered = 2.343
+Enter double: -23.4
+Double entered = -23.4
+Enter text: Hey!
+Text entered = Hey!`}</code></pre>
+
+        <p>As mentioned, there are other several ways to get input from the user. To learn more about <code>Scanner</code>, visit <a href="/java-programming/scanner">Java Scanner</a>.</p>
+
+      </div>
     </div>
   );
 }
--- src/app/docs/java/topics/Java-Fundamentals/Java-Expressions/page.jsx
+++ src/app/docs/java/topics/Java-Fundamentals/Java-Expressions/page.jsx
@@ -1,11 +1,277 @@
-export default function VariablePage() {
+export default function JavaExpressionsStatementsBlocksContent() {
   return (
-    <div className="prose prose-neutral max-w-none">
-      <h1 className="text-3xl font-bold mb-4">Java Variables</h1>
+    <div className="editor-contents text-black">
+      <h1 className="text-3xl font-bold mb-6">
+        Java Expressions, Statements and Blocks
+      </h1>
+      <div className="content space-y-4">
+        <p>
+          In previous chapters, we have used expressions, statements, and blocks
+          without much explaining about them. Now that you know about variables,
+          operators, and literals, it will be easier to understand these
+          concepts.
+        </p>
 
-      <p>
-       Welcome to java expressions
-      </p>
+        <hr className="my-6 border-gray-300" />
+
+        <h2 id="expressions" className="text-2xl font-semibold mt-6 mb-2">
+          Java Expressions
+        </h2>
+        <p>
+          A Java expression consists of{" "}
+          <a
+            href="/java-programming/variables-primitive-data-types#variables"
+            className="text-blue-600 underline"
+          >
+            variables
+          </a>
+          ,{" "}
+          <a
+            href="/java-programming/operators"
+            className="text-blue-600 underline"
+          >
+            operators
+          </a>
+          ,{" "}
+          <a
+            href="/java-programming/variables-primitive-data-types#literals"
+            className="text-blue-600 underline"
+          >
+            literals
+          </a>
+          , and method calls. To know more about method calls, visit{" "}
+          <a
+            href="/java-programming/methods"
+            className="text-blue-600 underline"
+          >
+            Java methods
+          </a>
+          . For example,
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`int score; 
+score = 90;`}</code>
+        </pre>
+
+        <p>
+          <code>score = 90</code> is an expression that returns an{" "}
+          <code>int</code>. Consider another example,
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`Double a = 2.2, b = 3.4, result;
+result = a + b - 3.4;`}</code>
+        </pre>
+
+        <p>
+          Here, <code>a + b - 3.4</code> is an expression.
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`if (number1 == number2)
+    System.out.println("Number 1 is larger than number 2");`}</code>
+        </pre>
+
+        <p>
+          Here, <code>number1 == number2</code> is an expression that returns a
+          boolean value. Similarly,{" "}
+          <code>"Number 1 is larger than number 2"</code> is a{" "}
+          <a
+            href="/java-programming/string"
+            className="text-blue-600 underline"
+          >
+            string
+          </a>{" "}
+          expression.
+        </p>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h2 id="statements" className="text-2xl font-semibold mt-6 mb-2">
+          Java Statements
+        </h2>
+        <p>
+          In Java, each statement is a complete unit of execution. For example,
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`int score = 9*5;`}</code>
+        </pre>
+
+        <p>
+          Here, we have a statement. The complete execution of this statement
+          involves multiplying integers <code>9</code> and <code>5</code> and
+          then assigning the result to the variable <code>score</code>.
+        </p>
+        <p>
+          In the above statement, we have an expression <code>9 * 5</code>. In
+          Java, expressions are part of statements.
+        </p>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h3
+          id="expression-statements"
+          className="text-xl font-semibold mt-6 mb-2"
+        >
+          Expression statements
+        </h3>
+
+        <p>
+          We can convert an expression into a statement by terminating the
+          expression with a <code>;</code>. These are known as expression
+          statements. For example,
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`// expression
+number = 10
+// statement
+number = 10;`}</code>
+        </pre>
+
+        <p>
+          In the above example, <code>number = 10</code> is an expression. By
+          adding a semicolon you convert it into a statement.
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`// expression
+++number
+// statement
+++number;`}</code>
+        </pre>
+
+        <p>
+          Similarly, <code>++number</code> is an expression whereas{" "}
+          <code>++number;</code> is a statement.
+        </p>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h3
+          id="declaration-statements"
+          className="text-xl font-semibold mt-6 mb-2"
+        >
+          Declaration Statements
+        </h3>
+        <p>
+          In Java, declaration statements are used for declaring variables. For
+          example,
+        </p>
+
+        <pre className="bg-gray-200 p-3 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">Double tax = 9.5;</code>
+        </pre>
+
+        <p>
+          The statement above declares a variable <var>tax</var> initialized to{" "}
+          <code>9.5</code>.
+        </p>
+
+        <div className="bg-yellow-100 p-4 rounded border border-yellow-300">
+          <strong>Note</strong>: There are control flow statements that are used
+          in decision making and looping in Java. You will learn about control
+          flow statements in later chapters.
+        </div>
+
+        <hr className="my-6 border-gray-300" />
+
+        <h2 id="blocks" className="text-2xl font-semibold mt-6 mb-2">
+          Java Blocks
+        </h2>
+        <p>
+          A block is a group of statements (zero or more) that is enclosed in
+          curly braces <code>{`{ }`}</code>. For example,
+        </p>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class Main {
+  public static void main(String[] args) {
+      
+      String band = "Beatles";
+      
+      if (band == "Beatles") { // start of block
+          System.out.print("Hey ");
+          System.out.print("Jude!");
+      } // end of block
+  }
+}`}</code>
+        </pre>
+
+        <p>
+          <strong>Output</strong>:
+        </p>
+        <pre className="bg-gray-200 p-3 rounded overflow-auto">
+          <code>Hey Jude!</code>
+        </pre>
+
+        <p>
+          In the above example, we have a block <code>if &#123;....&#125</code>.
+        </p>
+        <p>Here, inside the block we have two statements:</p>
+        <ul className="list-disc list-inside">
+          <li>
+            <code>System.out.print("Hey ");</code>
+          </li>
+          <li>
+            <code>System.out.print("Jude!");</code>
+          </li>
+        </ul>
+
+        <p>
+          However, a block may not have any statements. Consider the following
+          examples,
+        </p>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class Main {
+  public static void main(String[] args) {
+      
+      if (10 > 5) { // start of block    
+
+      } // end of block
+  }
+}`}</code>
+        </pre>
+
+        <p>
+          This is a valid Java program. Here, we have a block{" "}
+          <code>if &#123;....&#125</code>. However, there is no any statement
+          inside this block.
+        </p>
+
+        <pre className="bg-gray-200 p-4 rounded max-h-[600px] overflow-auto">
+          <code className="java hljs">{`class AssignmentOperator {
+  public static void main(String[] args) {  // start of block  
+
+  }  // end of block
+}`}</code>
+        </pre>
+
+        <p>
+          Here, we have block{" "}
+          <code>public static void main() &#123;....&#125</code>. However,
+          similar to the above example, this block does not have any statement.
+        </p>
+
+        <hr className="my-6 border-gray-300" />
+
+        <p>
+          <strong>Also Read:</strong>
+        </p>
+        <ul className="list-disc list-inside">
+          <li>
+            <a
+              href="/java-programming/if-else-statement"
+              className="text-blue-600 underline"
+            >
+              Java if…else Statement
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
